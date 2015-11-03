f2ec3627a010685a1eda3689662767367e337ae1
marinas <marinaserranomontes@gmail.com>
2015-11-03 11:37:26 +0100

    Adding performance profiler and update OpenTokConfig

--- OpenTokMeet/app/src/main/java/com/opentok/android/OpenTokConfig.java
+++ OpenTokMeet/app/src/main/java/com/opentok/android/OpenTokConfig.java
@@ -1,9 +1,11 @@
 package com.opentok.android;
 
-
 import java.net.MalformedURLException;
 import java.net.URL;
 
+/**
+ * This class contains API's, which are only used Internally.
+ */
 public class OpenTokConfig {
     static {
         System.loadLibrary("opentok");
@@ -58,10 +60,50 @@ public class OpenTokConfig {
         return subscriberStats;
     }
 
+    public static String getPublisherStat(PublisherKit publisher, long stream, String key){
+        return getPublisherStatNative(publisher, stream, key);
+    }
+
+    public static long[] getPublisherAudioStreams(PublisherKit publisher){
+        return getPublisherAudioStreamsNative(publisher);
+    }
+
+    public static long[] getPublisherVideoStreams(PublisherKit publisher){
+        return getPublisherVideoStreamsNative(publisher);
+    }
+
+    public static String getSubscriberStat(SubscriberKit subscriber, long stream, String key) {
+        return getSubscriberStatNative(subscriber, stream, key);
+    }
+
+    public static long[] getSubscriberAudioStreams(SubscriberKit subscriber){
+        return getSubscriberAudioStreamsNative(subscriber);
+    }
+
+    public static long[] getSubscriberVideoStreams(SubscriberKit subscriber){
+        return getSubscriberVideoStreamsNative(subscriber);
+    }
+
+    public static String getSDKVersion(Session session){
+        String sdkVersion = getSDKVersionNative(session);
+        return sdkVersion;
+    }
+
+    public static native void setProxy(String host, int port);
+    public static native void enableVP8HWDecoder (boolean vp8hwdecoder);
+
     protected static native void setAPIRootURLNative(String host, boolean ssl, int port, boolean rumorSSL);
     protected static native void setOTKitLogsNative(boolean otkitLogs);
     protected static native void setJNILogsNative(boolean jniLogs);
     protected static native void setWebRTCLogsNative(boolean webrtcLogs);
+
     protected static native String getPublisherInfoStatsNative(PublisherKit publisher);
     protected static native String getSubscriberInfoStatsNative(SubscriberKit subscriber);
+    protected static native String getPublisherStatNative(PublisherKit publisher,long stream, String key);
+    protected static native long[] getPublisherAudioStreamsNative(PublisherKit publisher);
+    protected static native long[] getPublisherVideoStreamsNative(PublisherKit publisher);
+    protected static native long[] getSubscriberAudioStreamsNative(SubscriberKit subscriber);
+    protected static native long[] getSubscriberVideoStreamsNative(SubscriberKit subscriber);
+    protected static native String getSubscriberStatNative(SubscriberKit subscriber, long stream, String key);
+    protected static native String getSDKVersionNative(Session session);
 }
